bb6795e58eaaf3e0498066ccd73958a0a5714145
alantothe <rubenamalpartida@gmail.com>
2023-08-17 15:04:54 -0400

    super basic python

---
+++ bouncer.py
@@ -0,0 +1,8 @@
+age = int(input("Just Stares"))
+
+if age < 18:
+    print("You're too young to get in.")
+elif age < 21:
+    print("You can come in, but you can't drink.")
+else:
+    print("Come on in. Don't make a mess.")
